75b8ebfd05e61d88ae6e47c76dde1ddbd4a9d952
Chris Martin <ch.martin@gmail.com>
2023-07-22 14:07:40 -0600

    fidget

--- fraction-optic/library/FractionOptic.hs
+++ fraction-optic/library/FractionOptic.hs
@@ -3,14 +3,59 @@ module FractionOptic where
 import Integer
 import Optics.Core
 
+
+---  Monomorphic fraction reduction  ---
+
+reducePositive ::
+  (Positive, Positive) ->
+  (Positive, Positive)
+reducePositive (a, b) =
+  let c = greatestCommonFactor (a, b)
+   in (quot a c, quot b c)
+
+
+---  The class  ---
+
 class NonzeroMagnitudeAffineTraversal a where
   nonzeroMagnitude :: AffineTraversal' a Positive
+
+
+---  Polymorphic fraction reduction  ---
+
+reduce :: NonzeroMagnitudeAffineTraversal a => (a, Positive) -> (a, Positive)
+reduce (x, y) =
+  case preview nonzeroMagnitude x of
+
+    Nothing -> (x, 1)
+
+    Just xMagnitude -> (xReduced, yReduced)
+      where
+        (xMagnitudeReduced, yReduced) =
+          reducePositive (xMagnitude, y)
+
+        xReduced =
+          set nonzeroMagnitude xMagnitudeReduced x
+
+greatestCommonFactor :: (Positive, Positive) -> Positive
+greatestCommonFactor (a, b) =
+  case narrow (remainder a b) of
+    Nothing -> b
+    Just c -> greatestCommonFactor (b, c)
+
+remainder :: Positive -> Positive -> Natural
+remainder a b = rem (convert a) (convert b)
+
+
+---  Instances  ---
 
 instance NonzeroMagnitudeAffineTraversal Positive where
   nonzeroMagnitude = castOptic simple
 
 instance NonzeroMagnitudeAffineTraversal Natural where
   nonzeroMagnitude = castOptic (prism' convert narrow)
+
+instance NonzeroMagnitudeAffineTraversal Integer where
+  nonzeroMagnitude = iso convert convert % nonzeroMagnitude @Signed
 
 instance NonzeroMagnitudeAffineTraversal Signed where
   nonzeroMagnitude = nonzero % _2
@@ -23,23 +68,3 @@ nonzeroReview (s, p) = NonZero s p
 
 nonzeroPreview :: Signed -> Maybe (Sign, Positive)
 nonzeroPreview = \case Zero -> Nothing; NonZero s p -> Just (s, p)
-
-instance NonzeroMagnitudeAffineTraversal Integer where
-  nonzeroMagnitude = iso convert convert % nonzeroMagnitude @Signed
-
-reduce :: NonzeroMagnitudeAffineTraversal a => (a, Positive) -> (a, Positive)
-reduce (x, y) =
-  case preview nonzeroMagnitude x of
-    Nothing -> (x, 1)
-    Just m ->
-      let d = greatestCommonFactor m y
-      in (set nonzeroMagnitude (quot m d) x, quot y d)
-
-remainder :: Positive -> Positive -> Natural
-remainder a b = rem (convert a) (convert b)
-
-greatestCommonFactor :: Positive -> Positive -> Positive
-greatestCommonFactor a b =
-  case narrow (remainder a b) of
-    Nothing -> b
-    Just c -> greatestCommonFactor b c
